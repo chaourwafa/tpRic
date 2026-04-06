052478795b50b55b285d341785d5c454a16ddb1e
Wafa <chaourwafa@gmail.com>
2026-04-06 21:50:18 +0200

    fix bug

--- generation_du_dataset.py
+++ generation_du_dataset.py
@@ -1,8 +1,15 @@
 import numpy as np
 
-np.random.seed(42)
+
 
 N = 2000
 
-#ginere les number
-X = np.random.uniform(-5, 5, (N, 2))  # shape: (2000, 2)
+#gineration de (x,y) 2000 point 
+X = np.random.uniform(-5, 5, (N, 2))  
+# definition de  la fonction f(x,y) = sin(x²+y²) +0.5 cos(2x+2y)
+def f(x,y) :
+    return np.sin(np.scrt(x**2 + y**2)) + 0.5* np.cos(2*x +2*y)
+ 
+z= f(X[:,0] , X[:,0])  
+
+z=z.rechape(2000 , 1)
