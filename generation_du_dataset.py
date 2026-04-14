4f76b0d07b9c8456b0d342d95a8307f8d52335f2
Wafa <chaourwafa@gmail.com>
2026-04-14 02:53:01 +0200

    update

--- generation_du_dataset.py
+++ generation_du_dataset.py
@@ -1,37 +1,68 @@
+
 import numpy as np
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-
-
+import plotly.graph_objects as go
+#----------------------------------
+#        dataset  :    
 
 N = 2000
 
-#gineration de (x,y) 2000 point 
-X = np.random.uniform(-5, 5, (N, 2)) 
-# definition de  la fonction f(x,y) = sin(x²+y²) +0.5 cos(2x+2y)
-def f(x,y) :
-    return np.sin(np.sqrt(x**2 + y**2)) + 0.5* np.cos(2*x +2*y)
- 
-z= f(X[:,0] , X[:,1])  
+X = np.random.uniform(-5, 5, (N, 2))
 
-z=z.reshape(2000 , 1)
+def f(x, y):
+    return np.sin(np.sqrt(x**2 + y**2)) + 0.5 * np.cos(2*x + 2*y)
 
-# normalisation  des points (x,y):
+z = f(X[:, 0], X[:, 1])
 
-mean =np.mean(X , axis=0 )
-std =np.std(X,axis=0)
+mean = np.mean(X, axis=0)
+std = np.std(X, axis=0)
 norm = (X - mean) / std
 
-# normalisation  des z:
-
-mean2 =np.mean(z)
-std2 =np.std(z)
+mean2 = np.mean(z)
+std2 = np.std(z)
 norm2 = (z - mean2) / std2
 
-#visulisation 
+fig = go.Figure(data=[
+    go.Scatter3d(
+        x=norm[:,0],
+        y=norm[:,1],
+        z=norm2,
+        mode='markers',
+        marker=dict(size=1)
+    )
+])
 
-fig =plt.figure()
-ax= fig.add_subplot(111 , projection='3d')
-ax.scatter(norm[:,0], norm[:,1], norm2, s=0.1)
-plt.savefig("plot3.png")
+fig.show()
+
+#------ parity 2:
+# couche 1
+W1 = np.random.randn(2, 64) * np.sqrt(2 / 2)
+b1 = np.zeros((1, 64))
+
+# couche 2
+W2 = np.random.randn(64, 64) * np.sqrt(2 / 64)
+b2 = np.zeros((1, 64))
+
+# sortie
+W3 = np.random.randn(64, 1) * np.sqrt(2 / 64)
+b3 = np.zeros((1, 1)) 
+
+def relu(x):
+    return np.maximum(0, x)
+
+# couche 1
+Z1 = norm @ W1 + b1
+A1 = relu(Z1)
+
+# couche 2
+Z2 = A1 @ W2 + b2
+A2 = relu(Z2)
+
+# sortie (linéaire)
+Z3 = A2 @ W3 + b3
+y_pred = Z3
+
+y_true = norm2.reshape(-1, 1)
+
+loss = np.mean((y_true - y_pred) ** 2)
+
+print("erreur =", loss)
